6edee8e805b188d926d88d73c4977ff2f67edb4f
k-makerir <kseniya.shloma@gmail.com>
2026-06-01 01:57:48 +0300

    update 6.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,24 @@
-'use strict'
-document.addEventListener("DOMContentLoaded", function(){
-    console.log('Скрипт отработал корректно')
-})
+const profileBtn = document.querySelector('.profile-btn');
+const modal = document.querySelector('#loginModal');
+const closeBtn = document.querySelector('.close-modal');
+
+profileBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    modal.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('active');
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.classList.remove('active');
+    }
+});
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        modal.classList.remove('active');
+    }
+});
